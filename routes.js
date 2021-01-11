8b2c897dfef24a53502f5aa1f665373a42901e9c
EfrainRodriguez <efrainrg92@gmail.com>
2021-01-11 15:16:30 -0300

    uploading project

---
+++ routes.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const routes = express.Router()
+
+routes.get('/', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('SELECT * FROM books', (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.json(rows)
+        })
+    })
+})
+
+routes.post('/', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('INSERT INTO books set ?', [req.body], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('book added!')
+        })
+    })
+})
+
+routes.delete('/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('DELETE FROM books WHERE id = ?', [req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('book excluded!')
+        })
+    })
+})
+
+routes.put('/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('UPDATE books set ? WHERE id = ?', [req.body, req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('book updated!')
+        })
+    })
+})
+
+module.exports = routes
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mysql = require('mysql')
+const myconn = require('express-myconnection')
+
+const routes = require('./routes')
+
+const app = express()
+app.set('port', process.env.PORT || 9000)
+const dbOptions = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'abc123',
+    database: 'library'
+}
+
+// middlewares -------------------------------------
+app.use(myconn(mysql, dbOptions, 'single'))
+app.use(express.json())
+
+// routes -------------------------------------------
+app.get('/', (req, res)=>{
+    res.send('Welcome to my API')
+})
+app.use('/api', routes)
+
+// server running -----------------------------------
+app.listen(app.get('port'), ()=>{
+    console.log('server running on port', app.get('port'))
+})
